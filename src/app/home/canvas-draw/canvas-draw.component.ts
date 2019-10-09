b9052f2db202017b98db94b01ff1f64f48b52aeb
chenxin <chen09@gmail.com>
2019-10-09 23:36:56 +0900

    no message

--- src/app/home/canvas-draw/canvas-draw.component.ts
+++ src/app/home/canvas-draw/canvas-draw.component.ts
@@ -12,6 +12,8 @@ export class CanvasDrawComponent implements OnInit, AfterViewInit {
     @ViewChild('myCanvas') canvas: ElementRef;
 
     canvasElement: any;
+    lastX: number;
+    lastY: number;
 
     constructor(public platform: Platform, public renderer2: Renderer2) {
     }
@@ -30,15 +32,37 @@ export class CanvasDrawComponent implements OnInit, AfterViewInit {
     }
 
     handleStart($event) {
-        console.log($event);
-
+        // console.log($event);
+        this.lastX = $event.touches[0].pageX;
+        this.lastY = $event.touches[0].pageY;
+        console.log('x:' + this.lastX + '\ty:' + this.lastY);
+        console.log('screenX:' + $event.touches[0].screenX + '\tscreenY:' + $event.touches[0].screenY);
+        // tslint:disable-next-line:max-line-length
+        console.log('dX:' + ($event.touches[0].pageX - $event.touches[0].screenX) + '\tdY:' + ($event.touches[0].pageY - $event.touches[0].screenY));
     }
 
     handleMove($event) {
-        console.log($event);
+        // console.log($event);
+
+        const ctx = this.canvasElement.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(this.lastX, this.lastY);
+        ctx.lineTo($event.touches[0].pageX, $event.touches[0].pageY);
+        ctx.closePath();
+        ctx.strokeStyle = '#000';
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        this.lastX = $event.touches[0].pageX;
+        this.lastY = $event.touches[0].pageY;
+
+        console.log('x:' + this.lastX + '\ty:' + this.lastY);
+        console.log('screenX:' + $event.touches[0].screenX + '\tscreenY:' + $event.touches[0].screenY);
+        // tslint:disable-next-line:max-line-length
+        console.log('dX:' + ($event.touches[0].pageX - $event.touches[0].screenX) + '\tdY:' + ($event.touches[0].pageY - $event.touches[0].screenY));
     }
 
     handleEnd($event) {
-        console.log($event);
+        // console.log($event);
     }
 }
